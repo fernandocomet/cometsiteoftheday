30de5032a0248eaddcc8fcac95d359f9e5c92a81
fernandocomet <fernandocomet@gmail.com>
2020-12-03 19:14:48 +0100

    CRA and add twitter widget

---
+++ cometsiteoftheday/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe src='https://www.sociablekit.com/app/embed/50081' frameborder='0' width='100%' height='1000'></iframe>
+      </header>
+    </div>
+  );
+}
+
+<script src='https://www.sociablekit.com/app/embed/twitter-hashtag-feed/widget.js' async defer></script>
+
+
+export default App;
